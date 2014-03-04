aeb1036378e467efb1f948f85293e46bb0c605ef
underscorephil <underscorephil@gmail.com>
2014-03-04 13:38:53 -0600

    Some pep changes and logics

--- SoftLayer/managers/cdn.py
+++ SoftLayer/managers/cdn.py
@@ -10,18 +10,20 @@ from SoftLayer.utils import IdentifierMixin
 
 class CDNManager(IdentifierMixin, object):
     """ Manage CCIs """
+
     def __init__(self, client):
         self.client = client
         self.account = self.client['Network_ContentDelivery_Account']
 
     def list_accounts(self):
         account = self.client['Account']
-        mask = 'cdnAccounts[%s]' % ', '.join(['id',
-                                              'createDate',
-                                              'cdnAccountName',
-                                              'cdnSolutionName',
-                                              'cdnAccountNote',
-                                              'status'])
+        mask = 'cdnAccounts[%s]' % ', '.join([
+            'id',
+            'createDate',
+            'cdnAccountName',
+            'cdnSolutionName',
+            'cdnAccountNote',
+            'status'])
         return account.getObject(mask=mask).get('cdnAccounts', [])
 
     def get_account(self, account_id, **kwargs):
@@ -37,9 +39,10 @@ class CDNManager(IdentifierMixin, object):
 
     def add_origin(self, account_id, media_type, origin_url, cname=None,
                    secure=False):
-        config = {'mediaType': media_type,
-                  'originUrl': origin_url,
-                  'isSecureContent': secure}
+        config = {
+            'mediaType': media_type,
+            'originUrl': origin_url,
+            'isSecureContent': secure}
 
         if cname:
             config['cname'] = cname
@@ -50,21 +53,13 @@ class CDNManager(IdentifierMixin, object):
         return self.account.deleteOriginPullRule(origin_id, id=account_id)
 
     def load_content(self, account_id, urls):
-        max_urls_per_call = 5
-
         if isinstance(urls, basestring):
             urls = [urls]
 
-        while urls:
-            self.account.loadContent(urls[0:max_urls_per_call], id=account_id)
-            urls = urls[max_urls_per_call:]
+        return self.account.loadContent(urls, id=account_id)
 
     def purge_content(self, account_id, urls):
-        max_urls_per_call = 5
-
         if isinstance(urls, basestring):
             urls = [urls]
 
-        while urls:
-            self.account.purgeCache(urls[0:max_urls_per_call], id=account_id)
-            urls = urls[max_urls_per_call:]
+            return self.account.purgeCache(urls, id=account_id)
